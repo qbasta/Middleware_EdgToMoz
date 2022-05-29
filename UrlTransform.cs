43d671a5e7564f89715e06963711322bf2fda8e2
Jakub Feliński <80295@student.pb.edu.pl>
2022-05-29 15:47:18 +0200

    Dodaj pliki projektów.

---
+++ UrlTransform.cs
@@ -0,0 +1,33 @@
+﻿namespace Middleware_EdgToMoz
+{
+    public class UrlTransform
+    {
+        private RequestDelegate _next;
+
+        public UrlTransform(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public Task Invoke(HttpContext context)
+        {
+            var r = context.Request.Headers["User-Agent"].ToString();
+
+            if (r.Contains("Edg"))
+            {
+                context.Response.Redirect("https://www.mozilla.org/pl/firefox/new/");
+            }
+
+            return _next(context);
+        }
+
+    }
+
+    public static class UrlTransformMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseUrlTransformMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<UrlTransform>();
+        }
+    }
+}
